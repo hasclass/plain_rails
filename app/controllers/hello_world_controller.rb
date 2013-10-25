db424b518507118b79f276c67e2364b11cd17066
Sebi Burkhard <sebi.burkhard@gmail.com>
2013-10-25 15:11:56 +0700

    fix

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,5 +1,5 @@
 class HelloWorldController < ApplicationController
   def home
-    render :text => "hello world from #{ENV["PWD"]}"
+    render :text => "hello world from #{ENV["PWD"].split("/")[2]}."
   end
 end
